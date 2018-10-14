c2112d3130ddf4e9b45dad2c5eb29dfc8222e55f
westewest <west@sd.keio.ac.jp>
2018-10-14 18:14:57 +0900

    delete generated TEDS

--- PlugFestRP/bluetooth-com/rfcommserver.py
+++ PlugFestRP/bluetooth-com/rfcommserver.py
@@ -13,7 +13,7 @@ import random
 
 parser = argparse.ArgumentParser(
     prog = 'alps.py',
-    usage = 'Receive BLE sensor data and send to NCAP with TEDS and METATEDS',
+    usage = 'Receive BLE sensor data and send to NCAP with TEDS and TXTTEDS (xml-based TEDS)',
     description= 'NCAP for TIM of ALPS Smart IoT BLE Sensor module\nYou have to install and communicate with supported TIM',
     epilog = 'Programmer: Hiroaki Nishi west@west.yokohama',
     add_help = True)
